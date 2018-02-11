647b0c2ab988d72fd449c2f8fbd9418d0181ff30
Utkarsh Sharma <Flamingice2801@users.noreply.github.com>
2018-02-11 13:55:52 +0530

    Add files via upload

---
+++ Final/FinalRecog.ino
@@ -0,0 +1,24 @@
+int sound = 8;
+
+
+void setup() 
+{
+
+  pinMode (sound, INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  int beat = digitalRead (sound);
+  
+  if (beat == 1)
+  {
+    Serial.println("1");
+  }
+  
+  
+}
+
+
